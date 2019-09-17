33777e36a34cb0034057e4d0a315c21d36d2e5da
HathePhoby <ketro156@gmail.com>
2019-09-17 10:32:00 +0100

    Cart functionality, clear, add, remove items

--- src/context.js
+++ src/context.js
@@ -7,7 +7,7 @@ class ProductProvider extends Component {
     state = {
         products: [],
         detailsProduct: detailProduct,
-        cart: storeProducts,
+        cart: [],
         modalOpen: false,
         modalProduct: detailProduct,
         cartSubTotal: 0,
@@ -58,17 +58,22 @@ class ProductProvider extends Component {
         const price = product.price;
         product.total = price;
 
-        this.setState(()=> {
-            return {products: tempProducts, cart:[...this.state.cart, product] };
-        }, ()=>{
-            console.log(this.state);
-            
+        this.setState(() => {
+            return {
+                products: tempProducts,
+                cart: [
+                    ...this.state.cart,
+                    product
+                ]
+            };
+        }, () => {
+            this.addTotals();
         });
     };
 
     openModal = id => {
         const product = this.getItem(id);
-        this.setState(() =>{
+        this.setState(() => {
             return {modalProduct: product, modalOpen: true};
         });
     }
@@ -79,24 +84,92 @@ class ProductProvider extends Component {
         });
     }
 
-    increment = id =>{
-        console.log("this is increment");
-    }
+    increment = id => {
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
 
-    decrement = id =>{
-        console.log("this is Decrement");
-    }    
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
 
-    removeItem = id =>{
-        console.log("this is remove");
-        
-    }
+        product.count = product.count + 1;
+        product.total = product.count * product.price;
+        this.setState(() => {
+            return {
+                cart: [...tempCart]
+            }
+        }, () => {
+            this.addTotals()
+        });
+    };
+
+    decrement = id => {
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count - 1;
+
+        if (product.count === 0) {
+            this.removeItem(id);
+        } else {
+            product.total = product.count * product.price;
+            this.setState(() => {
+                return {
+                    cart: [...tempCart]
+                }
+            }, () => {
+                this.addTotals()
+            });
+        }
+    };
+
+    removeItem = id => {
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+
+        tempCart = tempCart.filter(item => item.id !== id);
+
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(() => {
+            return {
+                cart: [...tempCart],
+                products: [...tempProducts]
+            };
+        }, () => {
+            this.addTotals();
+        });
+    };
 
     clearCart = () => {
-        console.log("Cart clear");
-        
+        this.setState(() => {
+            return {cart: []}
+        }, () => {
+            this.setProducts();
+            this.addTotals();
+        });
     }
 
+    addTotals = () => {
+        let subTotal = 0;
+        this
+            .state
+            .cart
+            .map(item => (subTotal += item.total));
+        const tempTax = subTotal * 0.1;
+        const tax = parseFloat(tempTax.toFixed(2));
+        const total = subTotal + tax;
+        this.setState(() => {
+            return {cartSubTotal: subTotal, cartTax: tax, cartTotal: total}
+        })
+    }
     render() {
         return (
             <ProductContext.Provider
